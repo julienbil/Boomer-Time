4ed10fdf986971e6d1e7627b863521dfd96676db
julienbil <cococococococo>
2019-10-11 18:53:41 -0400

    Version unity du projet updated

--- Boomer Time/Assets/Scenes/Scripts/PlayerMovement.cs
+++ Boomer Time/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public int speed;
+    public Rigidbody2D rb;
     public float horizontalSpeed;
     public float verticalSpeed;
+    public bool canMove = true;
     public int acceleration;
     // Start is called before the first frame update
     void Start()
     {
-    
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalSpeed = Input.GetAxis("X");
-        verticalSpeed = Input.GetAxis("Y");
+        if (canMove)
+        {
+            horizontalSpeed = Input.GetAxis("Mouse X");
+            verticalSpeed = Input.GetAxis("Mouse Y");
+        }
+        rb.AddForce()
+
+    
     }
 }
