a325d9b0a435b9287546ed38e5703b98f63a1955
julienbil <julienbil38@live.fr>
2019-10-11 20:02:08 -0400

    Tentative de creation de powerup

--- Boomer Time/Assets/Scenes/Scripts/PowerUps.cs
+++ Boomer Time/Assets/Scenes/Scripts/PowerUps.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class PowerUps : MonoBehaviour
 {
+    public Sprite lawnmower, truck;
+    public GameObject player;
+    public bool isLawnmower;
     public bool isTruck;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.rigidbody.tag == "Powerup")
+        {
+            TurnIntoLawnmower();
+        }
+    }
+
+
+    void TurnIntoLawnmower()
+    {
+        isTruck = false;
+        player.GetComponent<SpriteRenderer>().sprite = lawnmower;
+        IEnumerator BeLawnmower()
+        {
+            isLawnmower = true;
+            yield return new WaitForSeconds(10);
+            isLawnmower = false;
+        }
+        StartCoroutine(BeLawnmower());
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
