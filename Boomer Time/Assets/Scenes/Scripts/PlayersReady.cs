d44de9a5f9c6b6928c06f6d4936197295bf683bd
Nicolasx08 <nic.pageau@hotmail.com>
2019-10-11 23:27:48 -0400

    ajout ui sauf pause cum

---
+++ Boomer Time/Assets/Scenes/Scripts/PlayersReady.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayersReady : MonoBehaviour
+{
+    public static bool P1Ready = false;
+    public static bool P2Ready = false;
+
+    public GameObject start1;
+    public GameObject start2;
+    public Text text1;
+    public Text text2;
+    public Text textWaiting;
+    public static bool ok = true;
+
+    void Update()
+    {
+        if (ok)
+        {
+            checkUp();
+        }
+
+    }
+
+    void checkUp()
+    {
+        if (P1Ready == true && P2Ready == true)
+        {
+            ok = false;
+            Time.timeScale = 1f;
+            text1.enabled = false;
+            text2.enabled = false;
+            textWaiting.enabled = false; 
+        }
+        else if (P1Ready == true && P2Ready == false)
+        {
+            Debug.Log("ok");
+            textWaiting.text = "En attente du joueur 2.";
+        }
+        else if (P1Ready == false && P2Ready == true)
+        {
+            textWaiting.text = "En attente du joueur 1.";
+        }
+    }
+    public void but1()
+    {
+        P1Ready = true;
+        start1.SetActive(false);
+    }
+    public void but2()
+    {
+        P2Ready = true;
+        start2.SetActive(false);
+    }
+
+}
