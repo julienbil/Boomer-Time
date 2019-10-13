3585788d3af47a67cbc77d530719684bc10696d2
charles <renaudcharles14@hotmail.com>
2019-10-12 20:05:47 -0400

    debut meteor

---
+++ Boomer Time/Assets/Scenes/Scripts/Meteor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    public SpriteRenderer sr;
+
+    public float radius0, radius1, maxTime, minTime, animationTime;
+
+    public CircleCollider2D col;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
