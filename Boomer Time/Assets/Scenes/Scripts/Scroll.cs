f0393ff1a08a53a02162365a7dd033717c2f4ed2
jean0314 <jean0314@live.ca>
2019-10-13 00:30:18 -0400

    this is scene #@21!

--- Boomer Time/Assets/Scenes/Scripts/Scroll.cs
+++ Boomer Time/Assets/Scenes/Scripts/Scroll.cs
@@ -6,7 +6,7 @@ public class Scroll : MonoBehaviour
 {
     public float speed;
     public GameObject camera;
-    public GameObject destroyer;
+    public GameObject destroyer1, destroyer2, destroyer3, destroyer4;
     public GameObject mur, mur2, spawner;
 
     // Start is called before the first frame update
@@ -19,7 +19,10 @@ public class Scroll : MonoBehaviour
     void Update()
     {
         camera.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
-        destroyer.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        destroyer1.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        destroyer2.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        destroyer3.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        destroyer4.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         mur.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         mur2.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
         spawner.GetComponent<SupaSpawna>().xPos += speed * Time.deltaTime;
