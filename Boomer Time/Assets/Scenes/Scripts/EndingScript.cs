18ccf5335d2a6c01d6e3b24ebcdd863351512fd1
Nicolasx08 <nic.pageau@hotmail.com>
2019-10-13 10:08:52 -0400

    no conflict ok (ending done cvs not done but almost)
    cum

---
+++ Boomer Time/Assets/Scenes/Scripts/EndingScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndingScript : MonoBehaviour
+{
+    public Button rejouerBut,quitterBut,menuBut;
+    public void rejouer()
+    {
+        if (startMenu.nbeDeJoueur)
+        {
+            SceneManager.LoadScene("SampleScene1P");
+        }
+        if (!startMenu.nbeDeJoueur)
+        {
+            SceneManager.LoadScene("Scene2P");
+        }
+    }
+    public void quitter()
+    {
+        Application.Quit();
+    }
+    public void menu()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
